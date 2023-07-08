7b02119a6cbc505b77fd73152dec547dd30f2e53
June <sungj1207@gmail.com>
2023-07-08 17:33:45 +0900

    비동기 - Callback & Promise 객체의 이해

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
+// function a(callback) {
+//   setTimeout(() => {
+//     console.log('A')
+//     callback()
+//   }, 1000)
+// }
+
+// callback = resolve
+// 실행을 보장하고 싶은 위치에서 사용
 function a() {
-  console.log('A')
+  // promise: 약속의 객체!
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log('A')
+      resolve('Hello A')
+    }, 1000)
+  })
 }
 function b() {
-  console.log('B')
+  // promise: 약속의 객체!
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log('B')
+      resolve('Hello B')
+    }, 1000)
+  })
 }
-a()
-b()
+function c() {
+  // promise: 약속의 객체!
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log('C')
+      resolve('Hello C')
+    }, 1000)
+  })
+}
+function d() {
+  // promise: 약속의 객체!
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log('D')
+      resolve('Hello D')
+    }, 1000)
+  })
+}
+
+// callback 지옥 해결
+async function test() {
+  const h1 = await a()
+  const h2 = await b()
+  const h3 = await c()
+  const h4 = await d()
+  console.log('Done!')
+  console.log(h1, h2, h3, h4)
+}
+test()
