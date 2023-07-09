ffa6b8d675b2e6337e2def200467b9a763079886
June <sungj1207@gmail.com>
2023-07-09 15:16:21 +0900

    비동기 - 예외처리(then, catch, fianlly)

--- main.js
+++ main.js
@@ -1,56 +1,36 @@
-// function a(callback) {
-//   setTimeout(() => {
-//     console.log('A')
-//     callback()
-//   }, 1000)
+function a(number) {
+  return new Promise((resolve, reject) => {
+    if (number > 4) {
+      reject()
+      return // A 출력 X
+    }
+    setTimeout(() => {
+      console.log('A')
+      resolve()
+    }, 1000)
+  })
+}
+// function test() {
+//   a(5)
+//     .then(() => {
+//       console.log('Resolve!')
+//     })
+//     .catch(() => {
+//       console.log('Reject!')
+//     })
+//     .finally(() => {
+//       console.log('Done!')
+//     })
 // }
 
-// callback = resolve
-// 실행을 보장하고 싶은 위치에서 사용
-function a() {
-  // promise: 약속의 객체!
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      console.log('A')
-      resolve('Hello A')
-    }, 1000)
-  })
-}
-function b() {
-  // promise: 약속의 객체!
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      console.log('B')
-      resolve('Hello B')
-    }, 1000)
-  })
-}
-function c() {
-  // promise: 약속의 객체!
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      console.log('C')
-      resolve('Hello C')
-    }, 1000)
-  })
-}
-function d() {
-  // promise: 약속의 객체!
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      console.log('D')
-      resolve('Hello D')
-    }, 1000)
-  })
-}
-
-// callback 지옥 해결
 async function test() {
-  const h1 = await a()
-  const h2 = await b()
-  const h3 = await c()
-  const h4 = await d()
-  console.log('Done!')
-  console.log(h1, h2, h3, h4)
+  try {
+    await a(1)
+    console.log('Resolve!')
+  } catch (error) {
+    console.log('Reject!')
+  } finally {
+    console.log('Done!')
+  }
 }
 test()
