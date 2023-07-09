b09d1a89960cc91da122ed17730c50544f99767d
June <sungj1207@gmail.com>
2023-07-09 16:01:36 +0900

    비동기 - API 비동기 처리 연습

--- main.js
+++ main.js
@@ -1,36 +1,39 @@
-function a(number) {
-  return new Promise((resolve, reject) => {
-    if (number > 4) {
-      reject()
-      return // A 출력 X
+// [Promise MDN]
+// 대기(pending): 이행하거나 거부되지 않은 초기 상태.
+// 이행(fulfilled): 연산이 성공적으로 완료됨.
+// 거부(rejected): 연산이 실패함.
+
+function fetchMovies(title) {
+  // 대기(pending): 이행하거나 거부되지 않은 초기 상태.
+  const OMDB_API_KEY = '7035c60c'
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.get(`https://omdbapi.com?apikey=${OMDB_API_KEY}&s=${title}`)
+      // resolve
+      // 이행(fulfilled): 연산이 성공적으로 완료됨.
+      resolve(res)
+    } catch (error) {
+      console.log(error.message)
+      // Reject
+      // 거부(rejected): 연산이 실패함.
+      reject(error.message)
     }
-    setTimeout(() => {
-      console.log('A')
-      resolve()
-    }, 1000)
   })
 }
-// function test() {
-//   a(5)
-//     .then(() => {
-//       console.log('Resolve!')
-//     })
-//     .catch(() => {
-//       console.log('Reject!')
-//     })
-//     .finally(() => {
-//       console.log('Done!')
-//     })
-// }
 
 async function test() {
   try {
-    await a(1)
-    console.log('Resolve!')
+    const res = await fetchMovies('frozen')
+    console.log(res)
   } catch (error) {
-    console.log('Reject!')
-  } finally {
-    console.log('Done!')
+    console.log(error)
   }
 }
 test()
+
+function hello() {
+  fetchMovies('jobs')
+    .then(res => console.log(res))
+    .catch(() => {console.log(error)})
+}
+hello()
